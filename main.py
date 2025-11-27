a6051571147fa58e24842b268c304cf4472aa7ad
poppyseedcake <w.makowiec@gmail.com>
2025-11-27 22:08:31 +0100

    dasdsas

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-def main():
-    print("Hello from kickoff!")
-    #
+from textual.app import App
+from textual import events
 
+class KickOff(App):
+    COLORS = [
+        "white",
+        "maroon",
+        "red",
+        "purple",
+        "fuchsia",
+        "olive",
+        "yellow",
+        "navy",
+        "teal",
+        "aqua",
+    ]
+
+    def on_mount(self) -> None:
+        self.screen.styles.background = "darkblue"
+
+    def on_key(self, event: events.Key) -> None:
+        if event.key.isdecimal():
+            self.screen.styles.background = self.COLORS[int(event.key)]
 
 if __name__ == "__main__":
-    main()
+    app = KickOff()
+    app.run()
